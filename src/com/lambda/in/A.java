7ffd6364434e93e576c2b4f4e7ef788a8215a79a
LokeshGhub111 <59791539+LokeshGhub111@users.noreply.github.com>
2022-09-19 21:49:37 +0530

    Update A.java

--- src/com/lambda/in/A.java
+++ src/com/lambda/in/A.java
@@ -4,7 +4,9 @@ public class A
 {
     public static void main(String[] args)
     {
-        System.out.println("wel come lambdas expression 07-09-2022");
+        System.out.println("wel come lambdas expression 07-09-2022---");
+        
+        //I have add --- in print statement
 
     }
 }
